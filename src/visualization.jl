ef12c379b1c2d24522d54525e8150827fbf2a56e
Marcos Gil <marcosgildeoliveira@gmail.com>
2023-04-18 11:16:33 -0300

    added docstrings

--- src/visualization.jl
+++ src/visualization.jl
@@ -20,14 +20,51 @@ function convert2image(ψ::AbstractArray{T,N};colormap=:hot,ratio=1) where {T,N}
     convert2image(abs2.(ψ);colormap=colormap,ratio=ratio)
 end
 
+"""
+    visualize(ψ::AbstractArray{T,2}; 
+        colormap=:hot,
+        ratio=1) where T
+
+Vizualize the beam described by array `ψ`. 
+
+The colors are given by `colormap`. The full catalogue can be found at https://juliagraphics.github.io/ColorSchemes.jl/stable/catalogue/.
+
+The image is rescaled by `ratio`.
+"""
 function visualize(ψ::AbstractArray{T,2}; colormap=:hot,ratio=1) where T
     convert2image(normalize(Array(ψ)),colormap=colormap,ratio=ratio)
 end
 
+"""
+    visualize(ψ::AbstractArray{T,3}; 
+        colormap=:hot,
+        ratio=1) where T
+
+Vizualize the beam described by array `ψ`.
+
+The third dimension is interpreted as defining different images, which are displayed in a row.
+
+The colors are given by `colormap`. The full catalogue can be found at https://juliagraphics.github.io/ColorSchemes.jl/stable/catalogue/.
+
+The image is rescaled by `ratio`.
+"""
 function visualize(ψ::AbstractArray{T,3}; colormap=:hot,ratio=1,normalize_by_first=false) where T
     convert2image(hcat(eachslice(normalize(Array(ψ),normalize_by_first=normalize_by_first),dims=3)...),colormap=colormap,ratio=ratio)
 end
 
+"""
+    visualize(ψ::AbstractArray{T,4}; 
+        colormap=:hot,
+        ratio=1) where T
+
+Vizualize the beam described by array `ψ`.
+
+The third and fourth dimensions are interpreted as defining different images, which are displayed in a matrix.
+
+The colors are given by `colormap`. The full catalogue can be found at https://juliagraphics.github.io/ColorSchemes.jl/stable/catalogue/.
+
+The image is rescaled by `ratio`.
+"""
 function visualize(ψ::AbstractArray{T,4}; colormap=:hot,ratio=1,normalize_by_first=false) where T
     vcat(visualize.( eachslice(Array(ψ),dims=4),colormap=colormap,ratio=ratio,normalize_by_first=normalize_by_first )...)
 end
@@ -36,6 +73,15 @@ function show_animation(ψs::AbstractArray{T,3}; colormap=:hot,ratio=1,fps=16) w
     Images.gif([convert2image(normalize(Array(ψ)),colormap=colormap,ratio=ratio) for ψ in eachslice(ψs,dims=3)],fps=fps)
 end
 
+"""
+    save_animation(ψs::AbstractArray{T,3}, path; colormap=:hot,ratio=1,fps=16) where T
+
+Save an animation of `ψs` at `path` with a framerate of `fps`.
+
+The colors are given by `colormap`. The full catalogue can be found at https://juliagraphics.github.io/ColorSchemes.jl/stable/catalogue/.
+
+The image is rescaled by `ratio`.
+"""
 function save_animation(ψs::AbstractArray{T,3}, path; colormap=:hot,ratio=1,fps=16) where T
     imgstack = [RGB{N0f8}.(convert2image(normalize(Array(ψ)),colormap=colormap,ratio=ratio)) for ψ in eachslice(ψs,dims=3)]
     #encoder_options = (crf=23, preset="medium")
