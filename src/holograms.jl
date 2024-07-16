a5062c4dc16e9a6c2458453d6d879aa9072d7df8
Marcos Gil <marcosgildeoliveira@gmail.com>
2024-07-16 15:14:47 -0300

    fixed deletion of inverse besselj1

--- src/holograms.jl
+++ src/holograms.jl
@@ -1,3 +1,16 @@
+function inverse(f, y, bracket)
+    find_zero(x -> f(x) - y, bracket)
+end
+
+function interpolated_inverse(f, domain, N=512)
+    xs = LinRange(f(domain[1]), f(domain[2]), N)
+    ys = [inverse(f, x, domain) for x ∈ xs]
+
+    LinearInterpolation(xs, ys)
+end
+
+inverse_besselj1 = interpolated_inverse(besselj1, (0, 1.84))
+
 function standardize(holo, max_modulation)
     m, M = extrema(holo)
     @. round(UInt8, max_modulation * (holo - m) / (M - m))
