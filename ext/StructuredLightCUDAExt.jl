2f30d7401fcc3d83debdc23d81cd08bee5b41f78
Marcos Gil <marcosgildeoliveira@gmail.com>
2024-07-28 14:41:21 -0300

    removed old kerr in extension

--- ext/StructuredLightCUDAExt.jl
+++ ext/StructuredLightCUDAExt.jl
@@ -4,7 +4,4 @@ using StructuredLight, CUDA
 
 StructuredLight.to_device(::CuArray, y::AbstractArray) = CuArray(y)
 
-using Tullio, KernelAbstractions, CUDA.CUFFT
-include("kerr_propagation.jl")
-
 end
